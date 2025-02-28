1942be3697d47722938ee74e81ccaa08c03a4d51
khowellmobile <khowellmobile@gmail.com>
2025-02-28 12:51:13 -0500

    Added some styling for peakmodal

--- frontend/ohio-house-scraper-app/src/components/PeakModal.js
+++ frontend/ohio-house-scraper-app/src/components/PeakModal.js
@@ -1,28 +1,72 @@
 import classes from "./PeakModal.module.css";
+import { useEffect, useState } from "react";
 
 const PeakModal = ({ repInfo, handleCloseModal }) => {
+    const [legislationList, setLegislationList] = useState();
+    const [committeeList, setCommitteeList] = useState();
+
+    useEffect(() => {
+        if (repInfo["legislation"] !== "") {
+            const legList = repInfo["legislation"].split("<newline>");
+            setLegislationList(legList);
+        }
+    }, [repInfo]);
+
+    useEffect(() => {
+        if (repInfo["committees"] !== "") {
+            const comList = repInfo["committees"].split(",");
+            setCommitteeList(comList);
+        }
+    }, [repInfo]);
+
     const handleClose = () => {
         handleCloseModal();
     };
 
+    const getInfo = (field) => {
+        if (field.trim() === "" || field === null) {
+            return "Information not ready";
+        }
+    
+        return field;
+    };
+    
     return (
         <div className={classes.modalOverlay}>
             <div className={classes.mainContainer}>
-                <button className={classes.closeButton} onClick={handleCloseModal}>
+                <button className={classes.closeButton} onClick={handleClose}>
                     Close
                 </button>
-                <p>Hometown: {repInfo["hometown"]}</p>
-                <p>Address: {repInfo["address"]}</p>
-                <p>Phone: {repInfo["phone"]}</p>
-                <p>Fax: {repInfo["fax"]}</p>
-                <p>Committees: {repInfo["committees"]}</p>
-                <p>Legislation: {repInfo["legislation"]}</p>
-                <p>Education: {repInfo["education"]}</p>
-                <p>Politics: {repInfo["politics"]}</p>
-                <p>Employment: {repInfo["employment"]}</p>
-                <p>Community: {repInfo["community"]}</p>
-                <p>Image Formula: {repInfo["image_formula"]}</p>
-                <p>Image Url: {repInfo["image_url"]}</p>
+                <div className={classes.infoListing}>
+                    <p>Hometown:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["hometown"])}</div>
+                    <p>Address:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["address"])}</div>
+                    <p>Phone:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["phone"])}</div>
+                    <p>Fax:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["fax"])}</div>
+                    <p>Committees:</p>
+                    <div className={classes.listItem}>{getInfo(repInfo["committees"])}</div>
+                    <p>Legislation:</p>
+                    <div className={classes.listItem}>
+                        {legislationList && legislationList.length > 0
+                            ? legislationList.map((val, index) => <div key={index}>{val}</div>)
+                            : "Information not ready"}
+                    </div>
+                    <p>Education:</p>
+                    <div className={classes.listItem}>{getInfo(repInfo["education"])}</div>
+                    <p>Politics:</p>
+                    <div className={classes.listItem}>{getInfo(repInfo["politics"])}</div>
+                    <p>Employment:</p>
+                    <div className={classes.listItem}>{getInfo(repInfo["employment"])}</div>
+                    <p>Community:</p>
+                    <div className={classes.listItem}>{getInfo(repInfo["community"])}</div>
+                    <p>Image Formula:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["image_formula"])}</div>
+                    <p>Image Url:</p>
+                    <div className={classes.singleItem}>{getInfo(repInfo["image_url"])}</div>
+                </div>
             </div>
         </div>
     );
