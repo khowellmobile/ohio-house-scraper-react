b740f2d8907c1b53147ce36a198ebbe0427ea2fe
khowellmobile <khowellmobile@gmail.com>
2025-03-17 10:16:32 -0400

    added media query for rem size and fixed issue with dropdown on smaller screen sizes

--- frontend/ohio-house-scraper-app/src/components/Body.js
+++ frontend/ohio-house-scraper-app/src/components/Body.js
@@ -40,8 +40,8 @@ const Body = () => {
         setLockSocket(true);
 
         
-        /* const socket = new WebSocket("ws://3.146.35.114:65432"); */
-        const socket = new WebSocket("ws://localhost:65432");
+        
+        const socket = new WebSocket("ws://localhost:50000");
 
         socket.onopen = () => {
             console.log("Connected to WebSocket server");
